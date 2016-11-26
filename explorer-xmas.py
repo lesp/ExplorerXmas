2c80471cc1b84d293b7abb65f9af7f70d8b6e0d4
Les Pounder <lespounder@gmail.com>
2016-11-26 16:03:12 +0000

    Updated code

--- explorer-xmas.py
+++ explorer-xmas.py
@@ -3,13 +3,18 @@ from time import sleep
 
 try:
     while True:
+        print("Lights on")
         eh.output.one.on()
         sleep(5)
+        print("Lights off")
         eh.output.one.off()
         sleep(1)
+        print("Lights blink")
         eh.output.one.blink(0.5,0.5)
         sleep(5)
+        print("Lights pulse")
         eh.output.one.pulse(1,1,0.5,0.5)
+        sleep(5)
 except KeyboardInterrupt:
 	print("Exit")
 	eh.output.one.off()
